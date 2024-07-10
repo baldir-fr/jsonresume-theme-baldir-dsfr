726c7d6ef00bce04f4927332fe6d2783fb410e39
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-07-10 13:47:55 +0200

    handlebars skeleton

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+const Handlebars = require('handlebars');
+const {readFileSync, readdirSync} = require('fs');
+const {join} = require('path');
+
 const render = (resume) => {
-    return '<h1>' + resume.basics.name + '</h1>'
-}
-module.exports = {
-    render
-}
+    const css = readFileSync(`${__dirname}/style.css`, 'utf-8');
+    const tpl = readFileSync(`${__dirname}/resume.hbs`, 'utf-8');
+    const partialsDir = join(__dirname, 'theme/partials');
+
+    const filenames = readdirSync(partialsDir);
+    filenames.forEach((filename) => {
+        const matches = /^([^.]+).hbs$/.exec(filename);
+        if (!matches) return;
+        const name = matches[1];
+        const filepath = join(partialsDir, filename);
+        const template = readFileSync(filepath, 'utf8');
+        Handlebars.registerPartial(name, template);
+    });
+    return Handlebars.compile(tpl)({
+        css,
+        resume,
+    });
+};
+
+module.exports = {render};
